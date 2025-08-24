509c60409429b60175534af811ed86f078a44b54
Mokshagna <mokshagnayenduri@gmail.com>
2025-08-24 11:51:17 +0530

    create new spring boot project for basic login using spring security

---
+++ src/test/java/com/moksha/securityWithoutEncryption/SecurityWithoutEncryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moksha.securityWithoutEncryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityWithoutEncryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
