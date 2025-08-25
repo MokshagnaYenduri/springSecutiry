e180badc1ee40cf30cdc130d2ee6fcf0e0e106a0
Mokshagna <mokshagnayenduri@gmail.com>
2025-08-25 17:21:45 +0530

    upate register service in userService

--- src/main/java/com/moksha/securityWithoutEncryption/service/UserService.java
+++ src/main/java/com/moksha/securityWithoutEncryption/service/UserService.java
@@ -1,4 +1,15 @@
 package com.moksha.securityWithoutEncryption.service;
 
+import com.moksha.securityWithoutEncryption.model.Users;
+import com.moksha.securityWithoutEncryption.repo.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class UserService {
+
+    @Autowired
+    private UserRepo repo;
+
+    public Users register(Users user){
+        return repo.save(user);
+    }
 }
