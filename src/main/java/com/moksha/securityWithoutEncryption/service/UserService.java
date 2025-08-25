16509e3007344ce282fa5f9b759d327707473e1c
Mokshagna <mokshagnayenduri@gmail.com>
2025-08-25 22:41:05 +0530

    add encoding to the passwords saved in user service

--- src/main/java/com/moksha/securityWithoutEncryption/service/UserService.java
+++ src/main/java/com/moksha/securityWithoutEncryption/service/UserService.java
@@ -3,6 +3,7 @@ package com.moksha.securityWithoutEncryption.service;
 import com.moksha.securityWithoutEncryption.model.Users;
 import com.moksha.securityWithoutEncryption.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,8 +11,10 @@ public class UserService {
 
     @Autowired
     private UserRepo repo;
+    private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
 
     public Users register(Users user){
+        user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
     }
 }
