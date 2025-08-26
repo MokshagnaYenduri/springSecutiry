50e5192ee1bd0e868b8b2bcd33994cc71dc16f4f
Mokshagna <mokshagnayenduri@gmail.com>
2025-08-26 13:15:09 +0530

    modified authentication manager in security manager and login logic in use service'

--- src/main/java/com/moksha/securityWithoutEncryption/service/UserService.java
+++ src/main/java/com/moksha/securityWithoutEncryption/service/UserService.java
@@ -3,6 +3,9 @@ package com.moksha.securityWithoutEncryption.service;
 import com.moksha.securityWithoutEncryption.model.Users;
 import com.moksha.securityWithoutEncryption.repo.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -11,10 +14,20 @@ public class UserService {
 
     @Autowired
     private UserRepo repo;
+    @Autowired
+    private AuthenticationManager auth;
     private BCryptPasswordEncoder encoder = new BCryptPasswordEncoder(10);
 
     public Users register(Users user){
         user.setPassword(encoder.encode(user.getPassword()));
         return repo.save(user);
+    }
+
+    public String verify(Users user) {
+        Authentication aut;
+        aut = auth.authenticate(new UsernamePasswordAuthenticationToken(user.getName(), user.getPassword()));
+
+        if(aut.isAuthenticated()) return "success";
+        return "fail";
     }
 }
